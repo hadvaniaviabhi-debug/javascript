d78abe49ab89ecb65dadb61d9e97c2c008975f6f
hadvaniaviabhi-coder <hadvaniaviabhi@gmail.com>
2026-01-30 07:26:20 +0530

    [HA][change the array operation]

--- task.js
+++ task.js
@@ -35,3 +35,36 @@ console.log("convert course string into an array:-",Student.course.split(","));
 // display the lenght of student name
 console.log("length of student name:-", Student.name.length);
 
+//  ARRAY OPERATION    
+
+//add "swimming" to hobies
+Student.hobies.push("swimming");
+console.log("add swimming to hobies:-",Student.hobies);
+
+//remove the last hobies
+Student.hobies.pop();
+console.log("remove the last hobies:-",Student.hobies);
+
+// add "Drawing" at the beginning of hobies 
+Student.hobies.unshift("Drawing");
+console.log("add Drawing at the beginning of hobies :-",Student.hobies);
+
+// remove the frist hobies 
+Student.hobies.shift();
+console.log(" remove the frist hobies :-",Student.hobies);
+
+
+//merge hobies with course array
+
+//check if "react " exists in course array
+
+// find index of "js" in course 
+
+
+//extract  frist two mark
+
+
+// remove one failed subject mark(less than 40) using splice
+
+
+//convert hobies array into a string 
