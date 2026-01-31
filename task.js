ab38fac8ef4bf3a01acd5d7edf12ef65d388faac
hadvaniaviabhi-coder <hadvaniaviabhi@gmail.com>
2026-01-31 15:07:25 +0530

    [HA][added all file]

--- task.js
+++ task.js
@@ -1,76 +1,107 @@
-let Student ={
-    name : " Hadvani Akshi ",
-    email : "AKSHI@gmail.com",
-    course : "js,React,Node",
-    marks : [45,78,60,90,30],
-    hobies :["music","cricket","Reading"],
-    address : ["India",["Gujarat","Ahmedabad"]]
-}
+let Student = {
+    name : " first name ",
+    email : "USER@GMAIL.COM",
+    course : "JavaScript, React, Node",
+    marks : [45, 78, 60, 90, 33],
+    hobbies : ["Music", "Cricket", "Reading"],
+    address : ["India", ["Gujarat", "Ahmedabad"]]
+};
 
-//STRING OPERATION
+// STRING OPERATIONS //
+// remove space
+console.log("trim() : ",Student.name)
 
-// remove extra space from name and convert it to upppercase
+// convert upercase
+console.log("toUpperCase() : ",Student.name.toUpperCase())
 
- console.log("remove extra space :-",Student.name.trim())
- console.log("convert it to upppercase :-",Student.name.toUpperCase())
+// email lowercase
+console.log("toLowerCase() : ",Student.email.toLowerCase())
 
-// convert email to lowercase
+// check wether course includes
+console.log("includes() : ",Student.course.includes("React"))
 
- console.log("convert email to lowercase :-",Student.email.toLowerCase())
+// find the index of "Node" in course
+console.log("indexOf : ",Student.course.indexOf("Node"));
 
-//ckeck wether coures includes "react" 
-console.log("course includes React :-",Student.course.includes("React"));
-
-
-// find the index of "node" in course 
-console.log("index of Node:-",Student.course.indexOf("Node"));
-
-// replace "javascript" with "js"
-console.log("replace javascript with js:-",Student.course.replace("javascript", "js"));
-
+//replace "JavaScript" with "JS"
+console.log("replace() : ",Student.course.replace("JavaScript","JS"))
 
 // convert course string into an array
-console.log("convert course string into an array:-",Student.course.split(","));
+console.log("convert into array : ",Student.course.split(", "))
 
-// display the lenght of student name
-console.log("length of student name:-", Student.name.length);
+//display the length of student name
+console.log("length() : ",Student.name.length)
 
-//  ARRAY OPERATION    
+// ARRAY OPERATION //
+//add "Swimming" to hobbies
+Student.hobbies.push("Swimming");
+console.log(Student.hobbies);
 
-//add "swimming" to hobies
-Student.hobies.push("swimming");
-console.log("add swimming to hobies:-",Student.hobies);
+// remove the last hobby
+Student.hobbies.pop()
+console.log('remove last hobbie : ',Student.hobbies)
 
-//remove the last hobies
-Student.hobies.pop();
-console.log("remove the last hobies:-",Student.hobies);
+// add "Drawing" at the beginning of hobbies
+Student.hobbies.unshift("Drawing")
+console.log('beginning of hobbie : ',Student.hobbies)
 
-// add "Drawing" at the beginning of hobies 
-Student.hobies.unshift("Drawing");
-console.log("add Drawing at the beginning of hobies :-",Student.hobies);
+// remove the first hobby
+Student.hobbies.shift()
+console.log('remove first hobbie : ',Student.hobbies)
 
-// remove the frist hobies 
-Student.hobies.shift();
-console.log(" remove the frist hobies :-",Student.hobies);
+// merge hobbies with course
+const Student1 = {
+    hobbies : ["Music", "Cricket", "Reading"],
+    course : "JavaScript, React, Node"
+};
+const mergeArray = [...Student.hobbies, ...Student.course.split(", ")];
+console.log(mergeArray)
+
+// check if "React" exits in course array (case-insensitive)
+// console.log("check React : ",Student.course.includes("React"))
+let hasReact = courseArray
+.map(c => c.toLowerCase())
+.includes("React");
+
+// find index of "JS" in course array
+console.log("index of JS : ",Student.course.includes("JS"))
+
+// extract first two marks
+
+// remove one failed subject mark (less than 40) using splice
+
+// convert hobbies array into a string
+console.log("convert array into srting : ",Student.hobbies.join(", "))
+
+// ARRAY ITERATION METHOD //
+console.log("array iteration method :")
+let marks = [45, 78, 60, 90, 33];
+
+// use map() to add 5 bounds marks to each mark
+console.log("add 5 bounds marks : ",marks.map(num => num + 5))
+console.log("old array : ",marks)
+
+// use filter() to get only passed marks (>=40)
+console.log("passed marks (>=40) : ",marks.filter(num => num > 40))
+console.log("old array : ",marks)
+
+// use finnd() to find the first mark above 80
+console.log("first mark above 80 : ", marks.find(num => num > 80));
+
+// use forEach() to print each hobby
+console.log("print hobbies : ")
+Student.hobbies.forEach(hobby => console.log(hobby))
+
+// sort mark in ascending order
+console.log("sort mark in asceding order : ",marks.sort());
+
+// reverse mark order
+console.log("reverse mark ordeer : ",marks.reverse())
+
+// flatten the address array
 
 
-//merge hobies with course array
-console.log("merge hobies with course array:-",[...Student.hobies, ...Student.course.split(",")])
+// OBJECT HANDLING //
+// add a new property result = "Pass" if all marks are >=40
 
-
-//check if "react " exists in course array
-console.log(  Student.course.split(",").map(c => c.trim().toLowerCase()).includes("react")
-)
-
-
-// find index of "js" in course 
-console.log(" find index of js in course :-",Student.course.split(",").indexOf("js"))
-
-
-//extract  frist two mark
-
-
-// remove one failed subject mark(less than 40) using splice
-
-
-//convert hobies array into a string 
+// print final student object
